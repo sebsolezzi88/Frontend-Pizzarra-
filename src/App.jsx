b510270e2ef6a2bed687685169be755482d54ef6
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-27 19:55:42 -0300

    Creacion del navar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
 
 function App() {
   
 
   return (
-   
+    <>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
+    </>
    
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,11 @@
 const Login = () => {
   return (
     <>
-      <h1 className="bg-primary">Login</h1>
       <div className="container">
         <div className="row">
-          <div className="col-12 col-md-6 mx-auto">
+          <div className="col-12 col-md-6 mx-auto mt-5">
                 <form className="border border-3 border-primary  p-3 rounded shadow">
+                <legend className="text-primary text-center">Logueate para acceder a funciones</legend>
                   <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label text-uppercase">UserName</label>
                   <input type="email" class="form-control" id="username"></input>     
