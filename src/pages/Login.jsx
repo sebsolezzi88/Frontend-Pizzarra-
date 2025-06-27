76746a914ce3fa9fb7d25d45e1c648de4e555615
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-27 20:06:07 -0300

    cambar class por ClassName

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,15 +7,15 @@ const Login = () => {
           <div className="col-12 col-md-6 mx-auto mt-5">
                 <form className="bg-light border border-3 border-light  p-3 rounded shadow">
                 <legend className="text-dark text-center">Logueate para acceder a funciones</legend>
-                  <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label text-uppercase">UserName</label>
-                  <input type="email" class="form-control" id="username"></input>     
+                  <div className="mb-3">
+                  <label for="exampleInputEmail1" className="form-label text-uppercase">UserName</label>
+                  <input type="email" className="form-control" id="username"></input>     
                 </div>
-                <div class="mb-3">
+                <div className="mb-3">
                   <label for="exampleInputPassword1" class="form-label text-uppercase">Password</label>
-                  <input type="password" class="form-control" id="password"></input>
+                  <input type="password" className="form-control" id="password"></input>
                 </div>
-                <button type="submit" class="btn btn-dark text-uppercase d-block mx-auto">Login</button>
+                <button type="submit" className="btn btn-dark text-uppercase d-block mx-auto">Login</button>
               </form>
           </div>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,19 +8,19 @@ const Register = () => {
           <div className="col-12 col-md-6 mx-auto mt-5">
                 <form className="bg-light border border-3 border-light p-3 rounded shadow">
                 <legend className="text-dark text-center">Registrate para acceder a funciones</legend>
-                  <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label text-uppercase">UserName</label>
-                  <input type="email" class="form-control" id="username"></input>     
+                  <div className="mb-3">
+                  <label for="exampleInputEmail1" className="form-label text-uppercase">UserName</label>
+                  <input type="email" className="form-control" id="username"></input>     
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label text-uppercase">Password</label>
-                  <input type="password" class="form-control" id="password"></input>
+                <div className="mb-3">
+                  <label for="exampleInputPassword1" className="form-label text-uppercase">Password</label>
+                  <input type="password" className="form-control" id="password"></input>
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label text-uppercase">Repite Password</label>
-                  <input type="password" class="form-control" id="password2"></input>
+                <div className="mb-3">
+                  <label for="exampleInputPassword1" className="form-label text-uppercase">Repite Password</label>
+                  <input type="password" className="form-control" id="password2"></input>
                 </div>
-                <button type="submit" class="btn btn-dark text-uppercase d-block mx-auto">Registrarse</button>
+                <button type="submit" className="btn btn-dark text-uppercase d-block mx-auto">Registrarse</button>
               </form>
           </div>
         </div>
