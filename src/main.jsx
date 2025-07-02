bea03897c378fca027701699b9832217646819a5
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-02 09:57:25 -0300

    Envolver app en el provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
        <App />
+    </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
