325cdcf0ed405a0ad67f1b77e7842e20730cf4e0
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-02 10:38:04 -0300

    Agregada al home para obtener los post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react"
+import { getAllPost } from "../api/post";
 
 const Home = () => {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    const getPost = async () =>{
+      const response = await getAllPost();
+      console.log(response.posts);
+    }
+    getPost();
+
+  }, [])
+
   return (
     <>
         <h1>hoLA AL SISTIO</h1>
