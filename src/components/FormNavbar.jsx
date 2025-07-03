a8d61a3fe9a0d678e5f53bb9b556d4e46f662b21
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-03 11:05:16 -0300

    Comenzando fucionalidad del componente

--- src/components/FormNavbar.jsx
+++ src/components/FormNavbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormNavbar = () => {
+
+    //estadoInput 
+  const [inputSearch, setImputSearch] = useState('');
+
+  const handleChange = (e) =>{
+    setImputSearch(e.target.value);
+  }
+
+  const handletSubmit = (e) =>{
+    e.preventDefault();
+    console.log(inputSearch);
+  }
   return (
-    <form class="d-flex">
-        <input class="form-control me-sm-2" type="search" placeholder="Buscar usuario..."/>
+    <form onSubmit={handletSubmit} class="d-flex">
+        <input 
+        class="form-control me-sm-2" 
+        type="search" 
+        placeholder="Buscar usuario..."
+        value={inputSearch}
+        onChange={handleChange}
+        />
         <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
     </form>
   )
