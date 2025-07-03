24c83331ff6448d7f684c2695196be682b8a4ae7
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-03 19:41:40 -0300

    Diseño de userProfile

--- src/pages/ProfileUser.jsx
+++ src/pages/ProfileUser.jsx
@@ -1,11 +1,14 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { getPostByUsername } from '../api/post';
+import { searchUser } from '../api/follower';
+import { formatDate } from '../utils/utils';
 
 
 const ProfileUser = () => {
 
     const {username} = useParams();
+    const [userPost, setUserPost] = useState([]);
     const navigate = useNavigate();
 
     if(!username){
@@ -13,7 +16,23 @@ const ProfileUser = () => {
     }
 
     useEffect(() => {
-      
+
+      const handletEffect = async () =>{
+        //Verificar si existe el usuario
+            try {
+              const res = await searchUser(username);
+              if (res.status === 'success'){
+                //Si el usuario existe obtenemos sus post
+                const res = await getPostByUsername(username);
+                console.log(res.posts)
+                setUserPost(res.posts);
+              }
+            } catch (error) {
+              navigate("/");
+              console.log(error.response?.data || error.message);
+            }
+      }
+      handletEffect();
       
     }, [])
     
@@ -21,12 +40,42 @@ const ProfileUser = () => {
     return (
         
         <div className="container">
-            <div className="row">
-                <div className="col-4">
-                    <div className="bg-light">
-                        Nombre usuario
+            <div className="row mt-5">
+                
+                    <div className="col-10 col-sm-8 col-md-4 mx-auto mt-3">
+                        <div className="bg-light rounded p-4 shadow-sm">
+                            <h2 className="text-center mb-3">{username}</h2>
+                            <div className="d-flex justify-content-around">
+                                <span className='bi bi-people-fill'><strong> Seguidores:</strong> 15</span>
+                                <span><i className="bi bi-person-check-fill"></i> Siguiendo: 6</span>
+                            </div>
+                        </div>
                     </div>
+                <div className="col-11 col-md-6 mx-auto bg-light mt-3 p-4 rounded">
+                    <h5 className="text-center">
+                     Ultimos Post
+                    </h5>
+                   {userPost.length === 0 ? (
+                         <p className="text-center">No hay publicaciones.</p>
+                       ) : (
+                         userPost.map((post) => (
+                     <div key={post.id} className="mb-3 p-3 border rounded bg-white shadow-sm">
+                       <p className="mb-1">{post.content}</p>
+                       <div className="d-flex justify-content-between align-items-center">
+                         <small className="text-muted">
+                           Publicado el {formatDate(post.createdAt)}
+                         </small>
+                         <div>
+                           <span role="button" className="text-primary me-2" title="Ver comentarios">
+                             <i className="bi bi-chat"></i>
+                           </span>
+                         </div>
+                       </div>
+                     </div>
+                       ))
+                         )}
                 </div>
+
             </div>
         </div>
     )
