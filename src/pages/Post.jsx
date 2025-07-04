3aaf3c66e878642275284279daee044b1bb69c98
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-04 20:06:04 -0300

    Agrega comprovacion y mensaje de alerta

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getPostAndComments } from '../api/post';
 import { formatDate } from '../utils/utils';
+import Alert from '../components/Alert';
 
 
 const Post = () => {
@@ -9,6 +10,8 @@ const Post = () => {
 
     const [comments, setComments] = useState([]); //Estado de los comentarios
     const [userPost, setUserPost] = useState({}); //Estado del Usuario creador del post
+    const [inputComment, setInputCommnet] = useState(''); //Estado del imput del form
+    const [alert, setAlert] = useState({});
     
     useEffect(() => {
       const handletGet = async () =>{
@@ -25,13 +28,29 @@ const Post = () => {
       handletGet();
     }, [])
 
+    const handletChange = (e) =>{
+        setInputCommnet(e.target.value);
+    }
+    
+    const handletSubmit = async (e) =>{
+        e.preventDefault();
+        if(inputComment.trim() === ''){
+            setAlert({type:'danger',message:'Debe completa el campo'});
+            setTimeout(() => {
+            setAlert({})
+            }, 2000);
+            return
+        }
+        
+    }
+
     /* if (!userPost || !userPost.User) {
         return <p className="text-center mt-5">Cargando publicación...</p>;
     } */
     
   return (
    <div className="container">
-    <div className="row mt-5">
+    <div className="row mt-5 gap-1">
         <div className="col-12 col-md-6 bg-light rounded shadow-sm p-4 mx-auto mt-4">
             <h5 className="fw-bold mb-3">{userPost.User?.username}</h5>
 
@@ -40,13 +59,14 @@ const Post = () => {
             <p className="text-end text-secondary small mb-0">
                 Creado el: {formatDate(userPost.createdAt)}
             </p>
-
-            <form className="bg-dark border border-secondary border-2 p-3 mt-3 rounded">
+            {alert && <Alert type={alert.type} message={alert.message} />}
+            <form onSubmit={handletSubmit} className="bg-dark border border-secondary border-2 p-3 mt-3 rounded">
                 <textarea
                     className="form-control mb-3"
                     rows="3"
                     placeholder="Escribe tu comentario..."
-                ></textarea>
+                    onChange={handletChange}
+                >{inputComment}</textarea>
 
                 <div className="text-end">
                     <button className="btn btn-secondary">Comentar</button>
@@ -59,7 +79,7 @@ const Post = () => {
                 {comments.length === 0 ? <p className='text-center text-light fs-5'>No hay comentarios. Se el primero en comentar</p> : 
                     <p>Hay comentarios</p>
                 }
-        </div>
+            </div>
         </div>
 
     </div>
