e97dd344d1f18a531466f929734c46b773fd3e5a
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-04 10:54:10 -0300

    Agregado useEffect para obtener el post y los commets

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { getPostAndComments } from '../api/post';
 
 
 const Post = () => {
     const {idPost} = useParams();
+    
+    useEffect(() => {
+      const handletGet = async () =>{
+        try {
+            const res = await getPostAndComments(idPost);
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+      }
+      handletGet();
+    }, [])
+    
   return (
    <h3>{idPost} </h3>
   )
