8636fd4117aa7cbdcd36799831a8ed264c7ae299
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-04 11:18:14 -0300

    Agrego la visualizacion del post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,25 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getPostAndComments } from '../api/post';
+import { formatDate } from '../utils/utils';
 
 
 const Post = () => {
-    const {idPost} = useParams();
+    const {idPost} = useParams(); //id del Post para buscar y comentar
+
+    const [comments, setComments] = useState([]); //Estado de los comentarios
+    const [userPost, setUserPost] = useState({}); //Estado del Usuario creador del post
     
     useEffect(() => {
       const handletGet = async () =>{
         try {
             const res = await getPostAndComments(idPost);
-            console.log(res);
+            console.log(res.post);
+            setUserPost(res.post)
+            
         } catch (error) {
             console.log(error);
         }
       }
       handletGet();
     }, [])
+
+    /* if (!userPost || !userPost.User) {
+        return <p className="text-center mt-5">Cargando publicación...</p>;
+    } */
     
   return (
-   <h3>{idPost} </h3>
+   <div className="container">
+    <div className="row mt-5">
+        <div className="col-12 col-md-6 bg-light rounded p-2">
+            <h4 className='text-center'>{userPost.content}</h4>
+            <p>Por: <span>{userPost.User?.username}</span> </p>
+            <p>Creado el :  {formatDate(userPost.createdAt)} </p>
+        </div>
+        
+    </div>
+   </div>
   )
 }
 
